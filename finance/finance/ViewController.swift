cc9c615462f332fd7dfbac352d380b84365d8d0a
孟鑫 <mengxin@mengxindeMacBook-Air.local>
2015-06-28 13:12:16 +0800

    initialize project

---
+++ finance/finance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  finance
+//
+//  Created by 孟鑫 on 15/6/28.
+//  Copyright (c) 2015年 economy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ finance/financeTests/financeTests.swift
@@ -0,0 +1,36 @@
+//
+//  financeTests.swift
+//  financeTests
+//
+//  Created by 孟鑫 on 15/6/28.
+//  Copyright (c) 2015年 economy. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class financeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
